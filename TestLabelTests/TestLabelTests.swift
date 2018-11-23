5c306be36433fb3911ee6937697fe3bf52d22dc1
Alejandro Lujan <alejandro.lujan@mirada.tv>
2018-11-23 11:01:00 +0100

    Initial Commit

---
+++ TestLabelTests/TestLabelTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestLabelTests.swift
+//  TestLabelTests
+//
+//  Created by Alejandro Lujan on 23/11/2018.
+//  Copyright © 2018 Alejandro Lujan. All rights reserved.
+//
+
+import XCTest
+@testable import TestLabel
+
+class TestLabelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
